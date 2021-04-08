44db2728831f38e12da53659fc2c5c8b56e74588
Léon GALL <gallleon194@gmail.com>
2021-04-08 21:25:32 +0200

    Optimisation du code

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 // Page de code de l'application js d'animation des particules.
 // PW1 Léon GALL, Nicolas JARDINÉ
 
+
+
 // Accès au canvas
 var canvas = document.getElementById("affichage");
 var ctx = canvas.getContext("2d");
+
+
 
 // Générer un nombre entier entre min(inclus) et max(exclus)
 function genereNombre(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+
 
 // Générer une couleur en héxadécimal aléatoirement
 // Snippet récupéré sur https://stackoverflow.com/a/1484514
@@ -20,6 +26,9 @@ function getRandomColor() {
   }
   return color;
 }
+
+
+
 
 // Classe particule
 function Particule(masse, x, y) {
@@ -38,13 +47,14 @@ function Particule(masse, x, y) {
     this.radius = (masse / 500) * 5 + 5;
   } else {
     this.radius = (masse / 5000) * 10 + 7;
-  } // J'ai fait moi-même cette formule.
+  } // Donne le rayon d'une particule.
 
   this.color = getRandomColor();
 
   this.deplacement = true;
 
   this.draw = function () {
+    // Méthode qui dessine la particule
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
@@ -53,20 +63,24 @@ function Particule(masse, x, y) {
   };
 }
 
+
+
 // Initialisation d'un ensemble de particules
 
-var nbParticules_ini = 25;
+var nbParticules_ini = 25; // Lors du chargement de la page il y a 25 Particules.
 var tabParticules = new Array(nbParticules_ini);
 
 for (let i = 0; i < nbParticules_ini; i++) {
   let masse = genereNombre(10, 100);
   let rad = 0.0;
   let ang = (2 * Math.PI * i) / nbParticules_ini;
+
   if (masse < 50) {
     rad = genereNombre(50, 100);
   } else {
     rad = genereNombre(100, 200);
   }
+
   let x = canvas.width / 2 + rad * Math.sin(ang);
   let y = canvas.height / 2 + rad * Math.cos(ang);
 
@@ -80,15 +94,21 @@ for (let i = 0; i < nbParticules_ini; i++) {
   tabParticules[i] = nouvelleParticule;
 }
 
+
+
 // Ajout d'une particule de taille importante
 let centre_x = canvas.width / 2;
 let centre_y = canvas.height / 2;
 let grande_masse = 3000;
 
 var grandeParticule = new Particule(grande_masse, centre_x, centre_y);
+
 grandeParticule.deplacement = false;
 tabParticules.push(grandeParticule);
 grandeParticule.draw();
+
+
+
 
 // Ajout de particules par double clic sur le canvas
 canvas.addEventListener("dblclick", function () {
@@ -99,6 +119,7 @@ var valmasse = document.getElementById("masse");
 var valcolor = document.getElementById("color");
 
 function addParticule(event, particules) {
+  // Fonction qui crée la particule et l'affiche.
   var mouseX = event.pageX - canvas.offsetLeft;
   var mouseY = event.pageY - canvas.offsetTop;
 
@@ -109,37 +130,62 @@ function addParticule(event, particules) {
   particule_ajout.draw();
 }
 
+
+
+
 // Calcul du déplacement des particules
 var dt = 0.2;
 
 function calculDeplacements(particules, dt) {
+  // Fonction qui calcule le déplacement des particules.
   const G = 1;
-  for (let i = 0; i < particules.length; i++) {
+  for (let i = particules.length - 1; i >= 0; i--) {
+    // On parcourt le tableau de particules dans le sens inverse (permet de supprimer des éléments).
+    if (
+      particules[i].x > canvas.width ||
+      particules[i].x < 0 ||
+      particules[i].y > canvas.width ||
+      particules[i].y < 0
+    ) {
+      particules.splice(i, 1); // On supprime un élément qui n'est plus dans le canvas.
+      continue;
+    }
+
     if (particules[i].deplacement == false) {
       continue;
     }
+
     particules[i].force_x = 0;
     particules[i].force_y = 0;
+
     for (let j = 0; j < particules.length; j++) {
       if (i == j) {
         continue;
       }
+
       let dx = particules[j].x - particules[i].x;
       let dy = particules[j].y - particules[i].y;
+
       let r = Math.sqrt(dx * dx + dy * dy);
       let f = (G * particules[j].masse) / (r * r);
+
       particules[i].force_x += (f * dx) / r;
       particules[i].force_y += (f * dy) / r;
     }
+
     let ax = particules[i].force_x / particules[i].masse;
     let ay = particules[i].force_y / particules[i].masse;
+
     particules[i].velocite_x += ax * dt;
     particules[i].velocite_y += ay * dt;
+
     particules[i].x += particules[i].velocite_x * dt;
     particules[i].y += particules[i].velocite_y * dt;
   }
 }
-// todo Commenter, aérer et faire l'optionnel (supprimer les particules sorties du canvas)
+
+
+
 
 // Animations
 var intevalID;
@@ -148,18 +194,25 @@ function animer() {
     calculDeplacements(tabParticules, dt);
     ctx.fillStyle = "#171919";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     for (let p = 0; p < tabParticules.length; p++) {
       tabParticules[p].draw();
     }
   }, 10);
 }
 
+
+// Accès aux boutons
 var btnstart = document.getElementById("demarrer");
 var btnstop = document.getElementById("arreter");
 
 btnstart.addEventListener("click", function () {
+  // Animation lors du click sur démarrer.
+  clearInterval(intevalID); // Résoud le bug qui a lieu lors de double cliques sur le bouton démarrer (sinon, impossibilité d'arrêter l'animation si plusieurs cliques).
   animer();
 });
+
 btnstop.addEventListener("click", function () {
+  // Arrêt de l'animation lors du click sur arrêter.
   clearInterval(intevalID);
 });
